257aa8d256127b902aa34712d162ecc0e8d336d5
Alfriyadi Rafles <fradcareer@gmail.com>
2021-10-26 20:52:19 +0700

    Fixed image url on PageNotFound component

--- components/PageNotFound.vue
+++ components/PageNotFound.vue
@@ -1,7 +1,7 @@
 <template>
     <div>
         <h1>Page Not Found</h1>
-        <img :src="require('@/assets/images/fe25.jpg')" alt="">
+        <img :src="require('@/static/images/fe25.jpg')" alt="">
     </div>
 </template>
 
